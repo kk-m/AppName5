8d71804542b3a02728bf7a148a7b3c4919ee8642
河野 かおり <kono@mediba.jp>
2016-12-02 19:28:51 +0900

    とりあえずImageCellとdraeableに画像のやつ

---
+++ AppName5/Models/MyItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Prism.Mvvm;
+
+namespace AppName5
+{
+	public class MyItem : BindableBase
+	{
+		private string text;
+		public string Text
+		{
+			get { return text; }
+			set { SetProperty(ref text, value); }
+		}
+
+		private string note;
+		public string Note
+		{
+			get { return note; }
+			set { SetProperty(ref note, value); }
+		}
+
+		private string image;
+		public string Image {
+			get { return image;}
+			set { SetProperty(ref image, value);}
+		}
+
+		public MyItem()
+		{
+			
+		}
+	}
+}
--- AppName5/ViewModels/MainPageViewModel.cs
+++ AppName5/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using Prism.Mvvm;
+using Reactive.Bindings;
+//using AppName5.Models;
 
-namespace AppName5
+namespace AppName5.ViewModels
 {
 	public class MainPageViewModel : BindableBase
 	{
+		// ObservableCollection
+		public ReactiveCollection<MyItem> MyItems { get; } = new ReactiveCollection<MyItem>();
+
+
 		public MainPageViewModel()
 		{
-		}
+			MyItems.Add(new MyItem { Text = "text1", Note = "note1", Image="mine1.png" });
+			MyItems.Add(new MyItem { Text = "text2", Note = "note2", Image="mine2.png" });
+			MyItems.Add(new MyItem { Text = "text3", Note = "note3", Image="mine3.png" });
+}
 	}
 }
