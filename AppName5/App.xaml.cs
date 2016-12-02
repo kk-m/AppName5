16d43b818af1326483f5cb9dd2c71f460bd2327f
河野 かおり <kono@mediba.jp>
2016-12-02 16:55:29 +0900

    作ったばっか

---
+++ AppName5/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace AppName5
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppName5Page();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ AppName5/AppName5Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AppName5
+{
+	public partial class AppName5Page : ContentPage
+	{
+		public AppName5Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
