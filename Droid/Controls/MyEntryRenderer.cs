f19da5c36e001221f65e03d5492a772f0453db6b
河野 かおり <kono@mediba.jp>
2016-12-07 16:24:17 +0900

    For Custom : Custom Renderer 成功したぁ

---
+++ Droid/Controls/MyEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AppName5.Controls;
+using AppName5.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace AppName5.Droid.Controls
+{
+	public class MyEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				Control.SetBackgroundColor(global::Android.Graphics.Color.SteelBlue);
+				Control.SetTextColor(global::Android.Graphics.Color.DeepPink);
+			}
+		}
+	}
+}
---
+++ iOS/Controls/MyEntryRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AppName5.Controls;
+using AppName5.iOS.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace AppName5.iOS.Controls
+{
+	public class MyEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null) {
+				// あなたがここでUITextFieldにしたいことを何でもしてください！
+				Control.BackgroundColor = UIColor.FromRGB(255, 204, 204);
+				Control.BorderStyle = UITextBorderStyle.RoundedRect;
+			}
+		}
+	}
+}
